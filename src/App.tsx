6bfe62bece3f75f567f465668a532f894aa86e81
Gustavo Castro <gusncastro@gmail.com>
2023-07-12 15:44:19 -0300

    Fix retornando nulo

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,20 @@ function App() {
   const [oddsTime02, setOddsTime02] = useState("");
   const [apostaTime02, setApostaTime02] = useState("");
   const [ganhoTime02, setGanhoTime02] = useState("");
-  const [novaOdds3, setNovaOdds3] = useState("");
 
   useEffect(() => {
     const resultado = parseFloat(apostaTime01) * parseFloat(oddsTime01);
     const resultadoReal = resultado - parseFloat(apostaTime01);
     const oddsRetorno = resultado / resultadoReal;
     const resultadoTime2 = resultadoReal * oddsRetorno;
+
+    if (resultadoReal) {
+      setApostaTime02(resultadoReal.toFixed(2).toString());
+      setGanhoTime02(resultadoTime2.toFixed(2).toString());
+      setOddsTime02(oddsRetorno.toFixed(2).toString());
+    }
+
     setGanhoTime01(resultado.toFixed(2).toString());
-    setApostaTime02(resultadoReal.toFixed(2).toString());
-    setOddsTime02(oddsRetorno.toFixed(2).toString());
-    setGanhoTime02(resultadoTime2.toFixed(2).toString());
   }, [oddsTime01, apostaTime01]);
 
   return (
@@ -40,7 +43,7 @@ function App() {
             onChange={(e) => setApostaTime01(e.target.value)}
           />
           <span>
-            Aposta <h3>{apostaTime01 ? apostaTime01 : "0"}</h3>
+            Aposta <h3>{apostaTime01 || "0"}</h3>
           </span>
           <span>
             Ganho
@@ -55,6 +58,7 @@ function App() {
             <h3>{oddsTime01 && apostaTime01 ? ganhoTime01 : "0"}</h3>
           </span>
         </Caixa>
+
         <Caixa>
           <h3>Time 02</h3>
           <span>Odds para recuperar</span>
@@ -62,7 +66,7 @@ function App() {
           <span>Aposta para recuperar</span>
           <input type="number" disabled value={apostaTime02} />
           <span>
-            Aposta <h3>{apostaTime02}</h3>
+            Aposta <h3>{apostaTime02 || "0"}</h3>
           </span>
           <span>
             Ganho
